6b9edeb6f7e6ee18c5426bb156f4f993ec8a2ad7
taylus <therealbjohnson@gmail.com>
2018-06-26 19:59:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brandon/helloworld/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.brandon.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("url goes here");
+    }
+}
